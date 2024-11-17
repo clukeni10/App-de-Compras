078950ceaf13144502cadf9ef833e58e783b88b5
Carlos Lukeni <carloslukeni110@gmail.com>
2024-11-17 17:17:26 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+      const nome = document.getElementById("nome").value;
+      const valor = document.getElementById("valor").value;
+
+      if(nome && valor){
+            let Registros = JSON.parse(localStorage.getItem("Registros")) || [];
+
+            Registros.push({nome, valor});
+      
+            localStorage.setItem("Registros", JSON.stringify(Registros));
+            alert("Dados salvos com sucesso.")
+      } else{
+            alert("Insira todos os dados");
+      }
+      window.location.href = "two.html";
+});
+
